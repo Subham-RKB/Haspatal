bfad2cae14ebf465eb3c65ef2137392beed24e44
Subham-RKB <subhamchapagain45@gmail.com>
2021-08-12 23:00:10 +0545

    Debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter/rendering.dart';
+import 'package:haspatal/Pages/welcome.dart';
 import 'Pages/SignInPage.dart';
-=======
-import 'package:haspatal/pages/welcome.dart';
->>>>>>> 1c936efe37a6a99565e24fb127cafac5463acd37
 
 void main() async {
   //WidgetsFlutterBinding.ensureInitialized();
@@ -17,24 +14,15 @@ final Color bg = Color(0xFF434A5A);
 final Color pc = Color(0xFF7323A5);
 
 class MyApp extends StatelessWidget {
-<<<<<<< HEAD
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
       routes: {
-        '/': (_) => SignInPage(),
+        '/': (_) => Welcome(),
         //'/loginpage': (_) => loginpage(),
       },
-=======
-  const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Welcome(),
->>>>>>> 1c936efe37a6a99565e24fb127cafac5463acd37
     );
   }
 }
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,20 +1,62 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import './SignInPage.dart';
+import 'package:haspatal/main.dart';
 
 class Welcome extends StatelessWidget {
-
   Color bg = Color(0xff434A5A);
   Color wht = Color(0xffFFFFFF);
+  Widget _button(String textt) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60),
+      child: MaterialButton(
+        elevation: 0,
+        height: 80,
+        //minWidth: 70,
+        onPressed: () {
+          Navigator.push(
+            this.context,
+            MaterialPageRoute(builder: (context) => SignInPage()),
+          );
+
+          //startSignIn();
+          //if (_formKey.currentState.validate()) {
+          // print(email);
+          // print(password);
+          //dynamic result = await _auth.registerWithEmail(email, password);
+          //if (result == null) {
+          //setState(() => {error = 'Please supply a valid email'});
+          //}
+          //}
+        },
+        color: pc,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              textt,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 28, color: Colors.white, fontFamily: 'Red Rose'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor : bg, 
-      body : Column(
+      backgroundColor: bg,
+      body: Column(
         children: [
           Container(
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.6265, 
-            margin: EdgeInsets.fromLTRB(0,MediaQuery.of(context).size.height*0.3735, 0, 0),
+            height: MediaQuery.of(context).size.height * 0.6265,
+            margin: EdgeInsets.fromLTRB(
+                0, MediaQuery.of(context).size.height * 0.3735, 0, 0),
             decoration: BoxDecoration(
               color: wht,
               borderRadius: BorderRadius.only(
@@ -32,32 +74,33 @@ class Welcome extends StatelessWidget {
                       '\nWELCOME\nTO\nHASPATAL',
                       style: TextStyle(
                         fontSize: 40,
-                        fontFamily: 'RedRose',
-                        // fontWeight: FontWeight.bold,
+                        //fontFamily: 'RedRose',
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: ElevatedButton(
-                      child: Text(
-                        'GET STARTED',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                        ),
-                      ),
-                      onPressed: (){},
-                    ),
-                  ),
-                ),
+                SizedBox(height: 150),
+                _button("GET STARTED"),
+                // Align(
+                //   alignment: Alignment.bottomCenter,
+                //   child: Container(
+                //     margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                //     child: ElevatedButton(
+                //       child: Text(
+                //         'GET STARTED',
+                //         style: TextStyle(
+                //           fontSize: 30.0,
+                //         ),
+                //       ),
+                //       onPressed: () {},
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
         ],
-        
       ),
     );
   }
